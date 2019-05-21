e230872f71f7aac4af1041f7ffe9a2f93ef673e9
Mitchell Atlas <xatlasm@gmail.com>
2019-05-21 20:59:02 +0900

    Functioning textbox to add new todos
    Textbox adds new todo after list has been loaded.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <h1>{{title}}</h1>
-    <button v-on:click="getTodos">Get todo list</button>
+    <button v-on:click="getTodos">Load todo list</button>
     <form v-on:submit.prevent="newTodo">
       <input type="text" v-model="newTodoName" placeholder="New Todo">
     </form>
-    <ul v-if="todos" id="todo-list">
+    <ul id="todo-list">
       <li v-for="todo in todos" v-bind:key="todo.id">
         <p>{{todo.name}}</p>
       </li>
@@ -20,7 +20,7 @@ export default {
   data() {
     return {
       title: "Todo List",
-      todos: null
+      todos: {}
     }
   },
   methods: {
@@ -30,11 +30,12 @@ export default {
           this.todos = response.data
         })
     },
-    newTodo(todos, newTodoName) {
-      todos.push({
-        id: todos.length + 1,
-        name: newTodoName
+    newTodo() {
+      this.todos.push({
+        id: this.todos.length + 1,
+        name: this.newTodoName
       })
+      this.newTodoName = ''
     }
   }
 }
